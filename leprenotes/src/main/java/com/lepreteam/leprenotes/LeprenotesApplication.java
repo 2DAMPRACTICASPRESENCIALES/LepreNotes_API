47c40af2ac2e8f069b2cb4fed420daf17397ba52
FernandoCamposSegura <campossegura19@gmail.com>
2023-05-12 10:30:21 +0200

    Init

---
+++ leprenotes/src/main/java/com/lepreteam/leprenotes/LeprenotesApplication.java
@@ -0,0 +1,13 @@
+package com.lepreteam.leprenotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeprenotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeprenotesApplication.class, args);
+	}
+
+}
---
+++ leprenotes/src/test/java/com/lepreteam/leprenotes/LeprenotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lepreteam.leprenotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeprenotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
